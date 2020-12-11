9d6894adc4c6bc6dd05a0cbb7fd40ef96c696a01
Sean Kramer <seandominickramer@gmail.com>
2020-12-11 13:57:52 -0500

    Add tests for button click

--- src/components/ContactForm.test.js
+++ src/components/ContactForm.test.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import {render, screen} from '@testing-library/react';
 import ContactForm from './ContactForm';
+import userEvent from '@testing-library/user-event';
+import { act } from 'react-dom/test-utils';
 
 test('renders ContactForm without errors', () => {
   render(<ContactForm />);
@@ -23,4 +25,24 @@ test('input forms can be typed into and submitted', () => {
 render(<ContactForm />);
 
   const fNameInput = screen.getByLabelText(/first name/i);
+  const lNameInput = screen.getByLabelText(/last name/i);
+  const emailInput = screen.getByLabelText(/email/i);
+  const messageInput = screen.getByLabelText(/message/i);
+  const button = screen.getByRole("button", {name: /submit/i})
+
+  userEvent.type(fNameInput, 'se');
+  userEvent.type(lNameInput, 'kramer');
+  userEvent.type(emailInput, 'asda@dasd.com');
+  userEvent.type(messageInput, 'hi there');
+
+ act(() => {
+   userEvent.click(button);
+
+    const kramer = screen.getByText(/kramer/i);
+  
+  
+  })
+  expect(kramer).toBeInTheDocument();
+ 
+  
 })
