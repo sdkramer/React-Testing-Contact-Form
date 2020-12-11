91be11676d73b0cb6ed01322176667372100c79a
Sean Kramer <seandominickramer@gmail.com>
2020-12-11 13:19:22 -0500

    Add ids to input fields

--- src/components/ContactForm.test.js
+++ src/components/ContactForm.test.js
@@ -12,4 +12,15 @@ test('renders input labels without error', () => {
   const lName = screen.getByText(/last name*/i);
   const email = screen.getByText(/email*/i);
   const message = screen.getByText(/message/i);
+
+  expect(fName).toBeInTheDocument();
+  expect(lName).not.toBeNull();
+  expect(email).not.toHaveTextContent(/name/i);
+  expect(message).toHaveTextContent(/message/i)
+})
+
+test('input forms can be typed into and submitted', () => {
+render(<ContactForm />);
+
+  const fNameInput = screen.getByLabelText(/first name/i);
 })
